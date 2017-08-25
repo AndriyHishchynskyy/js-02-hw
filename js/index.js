bca7694cf74c4001e80cc0727cf94d1f0ac50945
Andriy Hishchynskyy <andriyhischynskyy@gmail.com>
2017-08-25 13:44:24 +0000

    OK 3

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let n = parseInt(prompt('Enter n') );
 function fib(n) {
   let a = 1,
     b = 1;
@@ -8,8 +9,28 @@ function fib(n) {
   }
   return b;
 } 
-function print(value){
-  document.write(value);
-  document.write("<br>");
+print(fib(n) );
+
+function fibRec(n) {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
 }
-print(fib(77) );
+print(fibRec(n) );
+
+function fibArr(n){
+  let arr = [];
+  let a = 1,
+    b = 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+    arr.push[c];
+  }
+  return b;
+}
+print(fibArr(n) );
+
+
+function print(value){
+  document.write(value, "<br>");
+}
